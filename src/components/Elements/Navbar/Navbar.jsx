63d17de300d35148650d56c9a19352c1ea503c3b
Ulil Albab <ulilalbabcmpk@gmail.com>
2025-05-03 08:42:02 +0700

    add link to homepage

--- src/components/Elements/Navbar/Navbar.jsx
+++ src/components/Elements/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,27 +11,34 @@ const Navbar = () => {
   return (
     <nav className="bg-slate-50 fixed top-0 left-0 right-0 p-4 lg:flex md:flex">
       <div className="container mx-auto flex justify-between items-center">
-        <a href="#" className='font-secondary italic text-xl'>Ulil</a>
-        <button
-          className="md:hidden"
-          onClick={toggleNavbar}
-        >
-          <i className={`bx ${isOpen ? 'bx-x' : 'bx-menu'}`}></i>
+        <a className="font-secondary italic text-xl">
+          <Link to="/">Ulil</Link>
+        </a>
+        <button className="md:hidden" onClick={toggleNavbar}>
+          <i className={`bx ${isOpen ? "bx-x" : "bx-menu"}`}></i>
         </button>
       </div>
-      <div className={`md:flex ${isOpen ? 'block' : 'hidden'}`}>
+      <div className={`md:flex ${isOpen ? "block" : "hidden"}`}>
         <ul className="flex flex-col items-center md:flex-row md:gap-5 md:space-x-4 mt-2 md:mt-0">
           <li>
-            <a href="#" className="hover:underline"><Link to="/">Design</Link></a>
+            <a href="#" className="hover:underline">
+              <Link to="/">Design</Link>
+            </a>
           </li>
           <li>
-            <a href="#" className="hover:underline"><Link to="/about">About</Link></a>
+            <a href="#" className="hover:underline">
+              <Link to="/about">About</Link>
+            </a>
           </li>
           <li>
-            <a href="https://www.linkedin.com/in/muhammad-ulil-albab-68786528a/" className="hover:underline">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/muhammad-ulil-albab-68786528a/" className="hover:underline">
+              LinkedIn
+            </a>
           </li>
           <li>
-            <a href="#" className="hover:underline">Resume</a>
+            <a href="#" className="hover:underline">
+              Resume
+            </a>
           </li>
         </ul>
       </div>
